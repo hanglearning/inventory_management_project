d572dc75296c1ed5743f83f6ded376956fe5406e
Hang DEV <hanglearning@gmail.com>
2017-04-01 14:38:35 -0400

    index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<Title>Register Make Money Together</Title>
+	<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+</head>
+<body>
+<h1>Create a new account</h1>
+	<p>
+		<strong>Email</strong>
+	</p>
+	<p>
+		<input type="text" placeholder="It will also be your username." required="">
+	</p>
+	<p>
+		<strong>Password</strong>
+	</p>
+	<p>
+		<input type="password" placeholder="DO NOT USE YOUR COMMON PASSWORD!" required="">
+	</p>
+	<p>
+		<strong>Phone Number</strong>
+	</p>
+	<p>
+		<input type="text" placeholder="US Phone number, required." required="">
+	</p>
+	<p>
+		<strong>QQ Account Info</strong>
+	</p>
+	<p>
+		<input type="text" placeholder="QQ is essential for communication." required="">
+	</p>
+	<p>
+		<strong>WeChat Account Info</strong>
+	</p>
+	<p>
+		<input type="text" placeholder="Optional">
+	</p>
+	<p>
+		<strong>Referred By</strong>
+	</p>
+	<p>
+		<input type="text" placeholder="Optional">
+	</p>
+	<p>
+		<button id="register">Register</button>
+	</p>
+</body>
+</html>
